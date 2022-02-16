d17ddacbc59dba286c99a53ea264f926ee1fd808
Bélanger Julien <2028413@cegepmontpetit.ca>
2022-02-16 15:03:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/personnedto/transfer/HomeItemResponse.java
@@ -0,0 +1,9 @@
+package com.example.personnedto.transfer;
+
+public class HomeItemResponse {
+    public Long id;
+    public String nom;
+    public String prenom;
+    public int age;
+    public String description;
+}
